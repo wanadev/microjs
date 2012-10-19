41899be10110761440f213d220e9d6ac3573d9f2
Yannick <ycomte@wanadev.fr>
2012-10-19 11:19:18 +0200

    Premier commit

---
+++ README.md
@@ -0,0 +1,64 @@
+Microjs 
+=======
+
+1. Qu'est ce que Microjs ?
+-----------------------
+
+Microjs (ujs) est une petite bibliothèque JavaScript qui se veut légère. 
+Le but d'ujs est de proposer des fonctionnalités minimales mais nécessaires, lors du développement d'une application web en JavaScript.
+
+2. Fonctionnalités
+------------------
+
+* Déclanchement manuel d'un évènement sur un élément du dom ;
+* Ajout/Suppression d'un attribut ;
+* Ajout/Suppression d'une classe ;
+* Ajout/Suppression d'un style ;
+* Ajax Get et Post (non compatible IE8 et inférieur) ;
+* Quelques alias.
+
+3. Exemples
+-----------
+
+## Ajax
+
+> ujs.ajax({
+>   method: "POST",
+>   url: "traitement.php",
+>   params: "action=1&user_id=45",
+>   success: function (response) {
+>     console.log(response); // retour serveur
+>   }
+> });
+
+## Déclanchement d'évènements
+
+> <div id="btnValidate" class="button red">Valider</div>
+> 
+> <script>
+>   // Récupération du bouton
+>   var button = ujs.getById("btnValidate");
+> 
+>   // Déclancher l'évenement click
+>   ujs.triggerEvent(button, "click");
+> </script>
+
+## Ajouter / Supprimer une classe
+
+> <div id="btnValidate" class="button red">Valider</div>
+> 
+> <script>
+>   // Récupération du bouton
+>   var button = ujs.getById("btnValidate");
+> 
+>   // Ajoute la classe selected si elle n'est pas déjà présente
+>   ujs.addClass(button, "selected");
+> 
+>   // Supprime la classe selected si elle est présente
+>   ujs.removeClass(button, "selected");
+> </script>
+
+4. Licence
+----------
+
+Licence BSD, voir le fichier LICENSE pour plus d'information
---
+++ ujs.js
@@ -0,0 +1,148 @@
+var ujs = window.ujs || {};
+
+(function ()
+{
+  // Déclanche un évènement sur un élèment du dom
+  ujs.triggerEvent = function (element, eventName)
+  {
+    if ((element[eventName] || false) && typeof element[eventName] == 'function')
+    {
+      element[eventName](element);
+    }
+  }
+  
+  // Ajouter une valeur à un attribut d'un élèment du dom
+  ujs.addOnAttribute = function (attribute, element, value)
+  {
+    var separator = attribute == "style" ? ";" : " ";
+    
+    var content = element.getAttribute(attribute);
+    var alreadyHere = false;  
+    var contentArray = [];
+    
+    if (content != null && content != "")
+    {
+      contentArray = content.split(separator);
+      
+      // A remplacer par un while
+      for (var i = 0, l = contentArray.length; i < l; i++)
+      {
+        if (contentArray[i] == value)
+          alreadyHere = true;
+      }
+    }
+    
+    if (!alreadyHere)
+      contentArray.push(value);
+    
+    element.setAttribute(attribute, contentArray.join(" "));
+  }
+  
+  // Supprimer une valeur à un attribut d'un élèment du dom
+  ujs.removeOnAttribute = function (attribute, element, value)
+  {
+    var separator = attribute == "style" ? ";" : " ";
+    
+    var content = element.getAttribute(attribute);
+    
+    if (content != null && content != "")
+    {
+      var contentArray = content.split(separator);
+      var size = contentArray.length;
+      var finalContent = [];
+      
+      for (var i = 0, l = contentArray.length; i < l; i++)
+      {
+        if (contentArray[i] != value)
+          finalContent.push(contentArray[i]);
+      }
+      
+      element.setAttribute(attribute, finalContent.join(separator));
+    }
+  }
+  
+  // Ajouter une classe à une élèment du dom (avec vérification)
+  ujs.addClass = function (element, className)
+  {
+    this.addOnAttribute("class", element, className);
+  }
+   
+  // Supprimer une classe à un élèment du dom (avec vérification)
+  ujs.removeClass = function (element, className)
+  {
+    this.removeOnAttribute("class", element, className);
+  }
+  
+  // Ajouter un style à un élèment du dom (avec vérification)
+  ujs.addStyle = function (element, styleName)
+  {
+    this.addOnAttribute("style", element, styleName);
+  }
+  
+  // Supprimer une classe à un élèment du dom (avec vérification)
+  ujs.removeStyle = function (element, styleName)
+  {
+    this.removeOnAttribute("style", element, styleName);
+  }
+  
+  ujs.ajax = function (parameters)
+  {
+    var url = parameters.url;
+    var method = parameters.method || "GET";
+    var params = parameters.params || "";
+    var callback = parameters.success || null;
+    
+    var xhr = new XMLHttpRequest();
+    
+    if (method == "POST")
+    {
+      xhr.open("POST", url, true);
+    
+      xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+      xhr.setRequestHeader("Content-length", params.length);
+      xhr.setRequestHeader("Connection", "close");
+      
+      xhr.onreadystatechange = function() 
+      {
+      	if(xhr.readyState == 4 && xhr.status == 200)
+        {
+          if (callback != null)
+            callback(xhr.responseText);
+        }
+      };
+      xhr.send(params);
+    }
+    else
+    {
+      xhr.open("GET", url + "?" + params, true);
+    
+      xhr.onreadystatechange = function()
+      { 
+          if(xhr.readyState == 4)  
+          {
+            if (callback != null)
+              callback(xhr.responseText);
+          }    
+      };
+      xhr.send(null);
+    }
+  }
+  
+    // Récupère un groupe d'élèment du dom par leur classe
+  ujs.getByClass = function (className)
+  {
+    return document.getElementsByClassName(className);
+  }
+  
+  // Récupère un élèment du dom par son Id
+  ujs.getById = function (id)
+  {
+    return document.getElementById(id);
+  }
+  
+  // Indique si la propriété est définie
+  ujs.isDef = function (element)
+  {
+    return typeof element != "undefined";
+  }
+})();
