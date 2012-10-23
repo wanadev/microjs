b8de2d61f8488154eae748e97664dcb8831d1a2f
Yannick <ycomte@wanadev.fr>
2012-10-23 09:34:41 +0200

    Ajout de la classe ArrayList avec un exemple
    Signed-off-by: Yannick <ycomte@wanadev.fr>

--- README.md
+++ README.md
@@ -6,6 +6,8 @@ Microjs
 Microjs (ujs) est une petite bibliothèque JavaScript qui se veut légère. 
 Le but d'ujs est de proposer des fonctionnalités minimales mais nécessaires, lors du développement d'une application web en JavaScript.
 
+Vous trouverez aussi quelques extensions pour la bibliothèque afin d'étendre ses fonctionnalités.
+
 ### 2. Fonctionnalités
 
 * Déclanchement manuel d'un évènement sur un élément du dom ;
@@ -13,7 +15,8 @@ Le but d'ujs est de proposer des fonctionnalités minimales mais nécessaires, lor
 * Ajout/Suppression d'une classe ;
 * Ajout/Suppression d'un style ;
 * Ajax Get et Post (non compatible IE8 et inférieur) ;
-* Quelques alias.
+* Quelques alias ;
+* Objet ArrayList (extension).
 
 ### 3. Exemples
 
@@ -60,6 +63,29 @@ ujs.addClass(button, "selected");
 // Supprime la classe selected si elle est présente
 ujs.removeClass(button, "selected");
 ```
+
+#### Utilisation de la classe ArrayList
+
+```javascript
+// nouvelle instance de ArrayList
+var list = new ujs.ArrayList(); 
+
+// Ajoute un tableau à la collection
+list.add([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+// Ajoute un seul élément à la fin de la collection
+list.add(45);
+
+// Supprime l'élément à la position 4
+list.removeAt(4);
+
+// Insère l'élément 42 à la position 4
+list.insertAt(4, 42);
+
+// Boucle sur tous les éléments de la collection
+list.forEach(function (el) { elements.push(el); });
+```
+
 ### 4. Licence
 
 Licence MIT, voir le fichier LICENSE pour plus d'information
---
+++ ujs.ArrayList.js
@@ -0,0 +1,164 @@
+var ujs = window.ujs || {};
+(function () 
+{
+	"use_strict";
+	var _members = [];
+	
+	ujs.ArrayList = function ()
+	{
+		this.length = 0;
+	}
+	
+	// Ajoute un élement ou un tableau d'élement à la collection
+	ujs.ArrayList.prototype.add = function (element)
+	{
+		var size = element.length;
+		
+		if (size > 0)
+		{
+			for (var i = 0; i < size; i++)
+				_members.push(element[i]);
+				
+			this.length = _members.length;
+		}
+		else
+			this.length = _members.push(element);
+	}
+	
+	// Compte le nombre d'élements dans la collection
+	ujs.ArrayList.prototype.count = function ()
+	{
+		return _members.length;
+	}
+	
+	// Vide le contenu de la collection
+	ujs.ArrayList.prototype.clear = function ()
+	{
+		_members = [];
+		this.length = 0;
+	}
+	
+	// Renvoie true si l'élement est contenu dans la collection
+	ujs.ArrayList.prototype.contains = function (element)
+	{
+		var i = 0, contains = false;
+		
+		while (i < this.length && !contains)
+		{
+			if (_members[i] == element)
+				contains = true;
+			i++;
+		}
+		
+		return contains;
+	}
+	
+	// Copie le contenu de la collection dans la tableau passé en paramètre
+	ujs.ArrayList.prototype.copyTo = function (array)
+	{
+		if (this.length > 0)
+		{
+			for (var i = 0; i < this.length; i++)
+				array.push(_members[i]);
+		}
+	}
+	
+	// Récupère l'élement à la position donnée
+	ujs.ArrayList.prototype.getAt = function (index)
+	{
+		if (index > this.length - 1 || index < 0)
+			return null;
+		
+		return _members[index];
+	}
+	
+	// Effectue une boucle et appel la fonction callback avec l'élement en cours
+	ujs.ArrayList.prototype.forEach = function (callback)
+	{
+		if (this.length > 0)
+		{
+			for (var i = 0; i < this.length; i++)
+				callback(_members[i]);
+		}
+	}
+	
+	// Retourne l'index de l'élement si il est présent dans la collection
+	ujs.ArrayList.prototype.indexOf = function (element)
+	{
+		var i = 0, index = -1;
+		
+		while (i < this.length && index == -1)
+		{
+			if (_members[i] == element)
+				index = i;
+			i++;
+		}
+		
+		return index;
+	}
+	
+	// Insert l'élément à la position donnée par index
+	ujs.ArrayList.prototype.insertAt = function (index, element)
+	{
+		if (index > 0 && index < this.length)
+		{
+			var tempArray = [];
+				
+			for (var i = 0; i < this.length; i++)
+			{
+				if (i == index)
+					tempArray.push(element);
+					
+				tempArray.push(_members[i])
+			}
+			
+			_members = tempArray;
+			this.length = _members.length;
+		}
+	}
+	
+	// Supprime un élement de la liste
+	ujs.ArrayList.prototype.remove = function (element)
+	{
+		var i = 0, done = false;
+		
+		while (i < this.length && !done)
+		{
+			if (_members[i] == element)
+			{
+				done = true;
+				this.removeAt(i);
+			}
+			i++;
+		}
+	}
+	
+	// Supprime un élement de la liste à la position donnée
+	ujs.ArrayList.prototype.removeAt = function (index)
+	{
+		if (index > 0 && index < this.length)
+		{
+			var tempArray = [];
+				
+			for (var i = 0; i < this.length; i++)
+			{
+				if (i != index && i < this.length)
+					tempArray.push(_members[i]);
+			}
+			
+			_members = tempArray;
+			this.length = _members.length;
+		}
+	}
+	
+	// Retourne une chaine de caractère de la liste
+	ujs.ArrayList.prototype.toString = function ()
+	{
+		var arrayString = "";
+		
+		for (var i = 0; i < this.length; i++)
+			arrayString += (i != 0 ? " - " : "") + _members[i];
+			
+		return arrayString;
+	}
+})();
