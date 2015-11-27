2b5982e03d4c4426b6e33d0b0be6a27f615f3671
Fabien LOISON <flo@flogisoft.com>
2015-11-27 17:19:37 +0100

    wip node package: ujs.js

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+module.exports = require("./ujs.js");
+module.exports.ArrayList = require("./ujs.ArrayList.js");
+module.exports.Dictionary = require("./ujs.Dictionary.js");
+module.exports.Math = require("./ujs.Math.js");
